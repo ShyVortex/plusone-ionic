be1b2493fef12911f46565d94bb6763f33fdbda6
ShyVortex <angelotr@ik.me>
2024-04-15 15:13:07 +0200

    Inizio pagina login

--- src/app/pages/registration/login/login.page.ts
+++ src/app/pages/registration/login/login.page.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonContent,
+  IonHeader, IonIcon,
+  IonImg,
+  IonInput,
+  IonItem,
+  IonLabel, IonList,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonImg, IonLabel, IonInput, IonItem, IonIcon, IonButton, IonList]
 })
 export class LoginPage implements OnInit {
 
