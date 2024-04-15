b9b930a5b27aa40f10acc22c34b520a4a5ed7820
ShyVortex <angelotr@ik.me>
2024-04-15 12:59:08 +0200

    Aggiunta altre pagine, light mode
    Aggiunte pagine per paziente, medico e infermiere, cambiato tema di default alla light mode

---
+++ src/app/pages/patient/sos/sos.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-sos',
+  templateUrl: './sos.page.html',
+  styleUrls: ['./sos.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class SOSPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
