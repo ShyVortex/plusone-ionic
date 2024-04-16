50de55bedefc4f80f4a78ecc813eb1cd08a5587f
ShyVortex <angelotr@ik.me>
2024-04-16 18:27:52 +0200

    Aggiunta header con impostazioni
    Impostate inoltre le routes per le varie pagine del paziente.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,18 +8,37 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/registration/login/login.page').then(m => m.LoginPage)
+    loadComponent: () => import('./pages/registration/login/login.page')
+      .then(m => m.LoginPage)
   },
   {
     path: 'signup',
-    loadComponent: () => import('./pages/registration/signup/signup.page').then(m => m.SignupPage)
+    loadComponent: () => import('./pages/registration/signup/signup.page')
+      .then(m => m.SignupPage)
   },
   {
     path: 'signup-continue',
-    loadComponent: () => import('./pages/registration/signup/signup-continue/signup-continue.page').then(m => m.SignupContinuePage)
+    loadComponent: () => import('./pages/registration/signup/signup-continue/signup-continue.page')
+      .then(m => m.SignupContinuePage)
   },
   {
     path: 'patient-home',
-    loadComponent: () => import('./pages/patient/home/home.page').then(m => m.HomePage)
+    loadComponent: () => import('./pages/patient/home/home.page')
+      .then(m => m.HomePage)
+  },
+  {
+    path: 'patient-notifs',
+    loadComponent: () => import('./pages/patient/notifications/notifications.page')
+      .then(m => m.NotificationsPage)
+  },
+  {
+    path: 'patient-reservation',
+    loadComponent: () => import('./pages/patient/reservation/reservation.page')
+      .then(m => m.ReservationPage)
+  },
+  {
+    path: 'patient-sos',
+    loadComponent: () => import('./pages/patient/sos/sos.page')
+      .then(m => m.SOSPage)
   },
 ];
--- src/app/pages/patient/sos/sos.page.ts
+++ src/app/pages/patient/sos/sos.page.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonImg,
+  IonTabBar,
+  IonTabButton, IonTabs,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-sos',
   templateUrl: './sos.page.html',
   styleUrls: ['./sos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon, IonImg, IonTabBar, IonTabButton, IonTabs]
 })
 export class SOSPage implements OnInit {
 
