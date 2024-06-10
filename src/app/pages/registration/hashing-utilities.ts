556c548ab06bfd03b7e95247bfb89ce05d021027
mariolino21714@gmail.com <mariolino21714@gmail.com>
2024-06-10 13:03:12 +0200

    Gestita logica per cambio password per tutti i ruoli

--- src/app/pages/registration/hashing-utilities.ts
+++ src/app/pages/registration/hashing-utilities.ts
@@ -3,7 +3,7 @@ import * as bcrypt from 'bcryptjs';
 
 export class HashingUtilities {
 
-  static  async  HashPassword(password: string):Promise<string> {
+  static  async HashPassword(password: string ):Promise<string> {
       const salt = await bcrypt.genSalt(10);
 
       const hashedPassword = await bcrypt.hash(password, salt);
--- src/app/pages/settings/security/security.page.ts
+++ src/app/pages/settings/security/security.page.ts
@@ -2,25 +2,28 @@ import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
-    IonButton,
-    IonCol,
-    IonContent, IonFooter,
-    IonGrid,
-    IonHeader,
-    IonImg, IonItem, IonLabel, IonRow, IonSelect, IonSelectOption, IonTabBar, IonTabButton, IonTabs, IonTextarea,
-    IonTitle,
-    IonToolbar,
+  AlertController,
+  IonButton,
+  IonCol,
+  IonContent, IonFooter,
+  IonGrid,
+  IonHeader,
+  IonImg, IonItem, IonLabel, IonRow, IonSelect, IonSelectOption, IonTabBar, IonTabButton, IonTabs, IonText, IonTextarea,
+  IonTitle,
+  IonToolbar,
 } from '@ionic/angular/standalone';
 import {NavController} from "@ionic/angular";
 import {LoginUtilities} from "../../registration/login/LoginUtilities";
 import {PersonaService} from "../../../services/PersonaService/persona.service";
+import {firstValueFrom} from "rxjs";
+import {HashingUtilities} from "../../registration/hashing-utilities";
 
 @Component({
   selector: 'app-security',
   templateUrl: './security.page.html',
   styleUrls: ['./security.page.scss'],
   standalone: true,
-    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonCol, IonGrid, IonImg, IonLabel, IonRow, IonSelect, IonSelectOption, IonTabBar, IonTabButton, IonTabs, IonTextarea, IonItem, IonFooter]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonCol, IonGrid, IonImg, IonLabel, IonRow, IonSelect, IonSelectOption, IonTabBar, IonTabButton, IonTabs, IonTextarea, IonItem, IonFooter, IonText]
 })
 
 export class SecurityPage implements OnInit {
@@ -30,20 +33,45 @@ export class SecurityPage implements OnInit {
   protected editableMail: boolean = false;
   protected editablePass: boolean = false;
   protected readonly LoginUtilities = LoginUtilities;
+  private hashedPassword:any
+  private password!:any
 
   /* Il punto esclamativo assicura a
   Ionic che la variabile sarà inizializzata prima dell'utilizzo */
-  @ViewChild('emailAreaRef') emailAreaRef!: IonTextarea;
   @ViewChild('passwordAreaRef') passwordAreaRef!: IonTextarea;
   @ViewChild('confirmPassRef') confirmPassRef!: IonTextarea;
+  protected confirmButtons = [
+    {
+      text: 'Annulla',
+      role: 'cancel',
+      handler: () => {}
+    },
+    {
+      text: 'Conferma',
+      role: 'confirm',
+      handler: async () => {
+       try{
+         this.hashedPassword.hashedPassword = await HashingUtilities.HashPassword(this.password)
+         console.log(this.hashedPassword.hashedPassword)
+         await firstValueFrom(this.personaService.updatePersonaPassword(this.persona.id,this.hashedPassword))
+         this.goToHome()
+       }
+       catch (error){
+         console.log('error', error)
+       }
+      }
+    }
+  ];
 
   constructor(
     private navCtrl: NavController,
     private personaService: PersonaService,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private alertController:AlertController
   ) {
     this.persona = personaService.getPersona();
     this.ruolo = "";
+    this.hashedPassword = {}
   }
 
   ngOnInit() {
@@ -56,20 +84,32 @@ export class SecurityPage implements OnInit {
     else if (LoginUtilities.getRuoloByEmail(this.persona.email) === 'ADMIN')
       this.ruolo = "ADMIN";
   }
+  async presentErrorAlert() {
+    const alert = await this.alertController.create({
+      header: 'Errore',
+      message: 'Le due password non corrispondono.',
+      buttons: ['OK'],
+    });
 
-  ngAfterViewInit() {
-    console.log('Readonly:', this.emailAreaRef.readonly);
+    await alert.present();
   }
+  async presentConfirmAlert() {
+    const alert = await this.alertController.create({
+      header: 'Conferma',
+      message: 'Confermare il cambiamento di password?',
+      buttons: this.confirmButtons,
+    });
+
+    await alert.present();
+  }
+
+
 
   navigateBack() {
     this.navCtrl.back();
   }
 
-  async editEmail() {
-    this.editableMail = true;
-    this.emailAreaRef.value = "";
-    this.cdr.detectChanges();
-  }
+
 
   async editPassword() {
     this.editablePass = true;
@@ -77,15 +117,7 @@ export class SecurityPage implements OnInit {
     this.cdr.detectChanges();
   }
 
-  async cancelEditEmail() {
-    this.emailAreaRef.value = this.persona.email;
-    this.editableMail = false;
-    this.cdr.detectChanges();
-  }
 
-  async confirmEditEmail() {
-
-  }
 
   async cancelEditPassword() {
     this.passwordAreaRef.value = "........";
@@ -93,8 +125,20 @@ export class SecurityPage implements OnInit {
     this.cdr.detectChanges();
   }
 
-  async confirmEditPassword() {
-
+  async confirmPassword() {
+      if(this.isMatching()){
+        this.password =  this.confirmPassRef.value
+        this.presentConfirmAlert()
+      }
+      else {
+        this.presentErrorAlert();
+      }
+  }
+  isDisable() :boolean{
+    return this.passwordAreaRef.value == '' || this.passwordAreaRef.value == undefined || this.confirmPassRef.value == "" || this.confirmPassRef.value == undefined;
+  }
+  isMatching() :boolean{
+    return this.passwordAreaRef.value === this.confirmPassRef.value
   }
 
   goToHome() {
@@ -103,7 +147,7 @@ export class SecurityPage implements OnInit {
     else if (this.ruolo === 'INFERMIERE')
       this.navCtrl.navigateBack("nurse-home");
     else if (this.ruolo === 'MEDICO')
-      this.navCtrl.navigateBack("patient-home");
+      this.navCtrl.navigateBack("medic-home");
     else if (this.ruolo === 'ADMIN')
       this.navCtrl.navigateBack("admin-home");
   }
